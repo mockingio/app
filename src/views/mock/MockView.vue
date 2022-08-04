a97c8ee461b0bd64cb3e829db062812acbdfc2b6
Khanh Chau <khanh.chaux@gmail.com>
2022-08-04 14:46:09 +0700

    fix: 🐛 route
    auto navigate to default mock/router incase params is missing

--- src/views/mock/MockView.vue
+++ src/views/mock/MockView.vue
@@ -11,18 +11,32 @@ import { useMockStore } from "@/stores";
 import { watch } from "vue";
 import { storeToRefs } from "pinia";
 import Header from "@/components/mock/header/Header.vue";
+import router from "@/router/index";
 
 const route = useRoute();
 const { fetchMocks, setActiveMock, setDefaultActiveMock } = useMockStore();
 const { mocks, error, activeMock } = storeToRefs(useMockStore());
 
 watch(
-  () => route.params.id,
-  (newId) => {
-    if (newId) {
+  () => [route.params.id, route.name, activeMock.value?.data.id],
+  ([newId, name, activeId]) => {
+    if (newId && newId !== activeId) {
       setActiveMock(newId as string);
-    } else {
+      return;
+    }
+
+    if (!activeId) {
       setDefaultActiveMock();
+      return;
+    }
+
+    if (!name) {
+      router.push({
+        name: "routes-view",
+        params: {
+          id: activeId as string,
+        },
+      });
     }
   }
 );
--- src/views/mock/RoutesView.vue
+++ src/views/mock/RoutesView.vue
@@ -8,32 +8,51 @@
 </template>
 
 <script setup lang="ts">
+import RouteDetail from "@/components/mock/route/RouteDetail.vue";
+import RouteList from "@/components/mock/route/RouteList.vue";
+import router from "@/router/index";
 import { useMockStore } from "@/stores";
 import { storeToRefs } from "pinia";
-import RouteList from "@/components/mock/route/RouteList.vue";
-import RouteDetail from "@/components/mock/route/RouteDetail.vue";
-import { useRoute } from "vue-router";
 import { provide, watch } from "vue";
+import { useRoute } from "vue-router";
 
 const { activeMock, activeRoute } = storeToRefs(useMockStore());
 const { setActiveRoute, setDefaultActiveRoute } = useMockStore();
 
 const route = useRoute();
+
 watch(
-  () => route.params.routeId,
-  (newId) => {
-    setRouteId(newId as string);
+  () => [route.params.routeId, route.name, activeMock.value?.data.id],
+  ([newId]) => {
+    // TODO: We should have the constant for all routeName
+    if (route.name === "routes-view") {
+      if (!route.params.routeId && route.params.id && activeRoute.value?.id) {
+        router.push({
+          name: "route-view",
+          params: {
+            id: route.params.id as string,
+            routeId: activeRoute.value.id as string,
+          },
+        });
+
+        return;
+      }
+    }
+
+    if (newId) {
+      setActiveRoute(newId as string);
+      return;
+    }
+
+    if (!newId && !activeRoute.value?.id) {
+      setDefaultActiveRoute();
+      return;
+    }
+  },
+  {
+    immediate: true,
   }
 );
-
-const setRouteId = (id?: string) => {
-  if (id) {
-    setActiveRoute(id as string);
-  } else {
-    setDefaultActiveRoute();
-  }
-};
-setRouteId(route.params.routeId as string);
 
 provide("mock", activeMock);
 provide("route", activeRoute);
